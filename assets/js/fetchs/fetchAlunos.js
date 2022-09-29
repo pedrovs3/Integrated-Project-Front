9960e6b449b32f0fc02398a6fec54433b19093e5
Pedro Vieira <pedrovs3@hotmail.com>
2022-09-29 10:51:55 -0300

    arrumando submenu

--- assets/js/fetchs/fetchAlunos.js
+++ assets/js/fetchs/fetchAlunos.js
@@ -1,5 +1,5 @@
 const openNewUrl = async (textContent) => {
-  
+
   const curso = textContent.toLowerCase();
   const url = `https://integrated-api.netlify.app/.netlify/functions/api/alunos/${curso}`
   const response = await fetch(url)
